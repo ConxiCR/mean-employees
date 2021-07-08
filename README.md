7bb09c7139e22af4063f47b82f07bcbafdb390ff
ConxiCr <conxicr@hotmail.com>
2021-07-08 11:11:15 +0200

    creacion middleware morgan para escuchar las peticiones y verlas en consola
    Signed-off-by: ConxiCr <conxicr@hotmail.com>

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # mean-employees
+Utilizando stack MEAN, desarrollo web basado en javascript. Podemos crear aplicaciones en frontend y backend(full stack) con la misma tecnologia.
+https://www.neoland.es/hs-fs/hubfs/mean_stack_tecnologico_neoland.jpg?width=1080&name=mean_stack_tecnologico_neoland.jpg
+1. Creamos Carpeta SERVER
+    - Iniciación del proyecto con Nodejs
+    - Instalación de paquetes y módulos(exprex y morgan(para ver las peticiones que llegan al servidor))
+    - Creación archivo Index.js
+    
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 //console.log("hello word");
 //requerimos express para poder crear un servidor(rutas)
-const express = require ('express')
-//llamada al servidor. Ejecuto express
+const express = require('express');
+const morgan = require('morgan')
+//ejecuto express
 const app = express()
-//exporto la app. Escucho en el puerto asignado
-app.listen(3000)
-//mensaje para saber que el servidor ya funciona
-console.log('Server on port', 3000)
+//utilizamos un middleware que es una función que permite procesar algo antes de que termine. Escucha las peticiones y las muestra en consola
+app.use(morgan('dev'))
+//llamo a la app. Creo un servidor en el puerto 3000
+app.listen(3000);
+console.log('Server on port', 3000);
